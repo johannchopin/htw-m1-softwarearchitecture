91f847b70f47b470cf8350d1b04dfc0b216c3932
Lyokolux <lyokolux@protonmail.com>
2021-01-01 14:58:45 +0100

    ✨ add a timestamp + refactor

--- Entry/fakeEmails.py
+++ Entry/fakeEmails.py
@@ -2,17 +2,20 @@ from faker import Faker
 from random import randint, choice
 from dataclasses import dataclass
 from typing import NewType, List
+from time import time
 
 FAKE = Faker()
+# Fake.seed()
 
 EmailAddress = NewType('EmailAdress', str)
 
 
-@dataclass
+@dataclass(frozen=True)
 class Email:
     """ Structure of an email """
     sender: EmailAddress
     reciever: EmailAddress
+    timestamp: int
     content: str
 
 
@@ -21,6 +24,7 @@ def generateEmailAdresses(amount: int) -> List[EmailAddress]:
 
 
 def generateEmailContent(nb_max_paragraph=3, nb_max_sentences_per_paragraph=10) -> str:
+    """ Generate the body of an email, i.e. the message """
     paragraph_amount = range(randint(1, nb_max_paragraph))
     return '\n'.join((FAKE.paragraph(
         nb_sentences=nb_max_sentences_per_paragraph,
@@ -28,11 +32,17 @@ def generateEmailContent(nb_max_paragraph=3, nb_max_sentences_per_paragraph=10) 
     ) for _ in paragraph_amount))
 
 
+def getTimestamp(precision_after_second=6):
+    return int(time() * 10**precision_after_second)
+
+
 def generateSimpleEmail(emailAdresses: List[EmailAddress]) -> Email:
+    """" Generate a fake email from a list of email address """
     sender = choice(emailAdresses)
     reciever = choice(emailAdresses)
     content = generateEmailContent(nb_max_paragraph=5)
-    return Email(sender, reciever, content)
+    timestamp = getTimestamp()
+    return Email(sender, reciever, timestamp, content)
 
 
 if __name__ == "__main__":
