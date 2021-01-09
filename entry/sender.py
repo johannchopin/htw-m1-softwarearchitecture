ee878ce529c6b2b363697ef9fd6e9c78d5ddba05
Lyokolux <lyokolux@protonmail.com>
2021-01-09 17:35:46 +0100

    recycle(sender.py): wrap the main logic into a run function

--- entry/sender.py
+++ entry/sender.py
@@ -5,38 +5,35 @@ from pathlib import Path
 from datetime import datetime
 from fakeEmails import Email, generateEmailAdresses, generateEmails
 
-
-def mkdir_if_not_exists(path: Path):
-    if not path.exists():
-        path.mkdir(parents=True)
-
-
-def get_latest_filename(path: Path) -> int:
-    return max(1, len(os.listdir(path)))
-
-
-def waitIfFileChunkLimitReached(path: Path, limit: int, sleep_time: int):
-    while True:
-        file_in_queue = len(os.listdir(DATA_PATH))
-
-        if file_in_queue >= limit:
-            print('Sleeping...')
-            time.sleep(sleep_time)
-        else:
-            return
+DATA_PATH = Path("./data")
+CHUNK_AMOUNT_LIMIT = 100
+EMAIL_ADRESS_AMOUNT = 100
+EMAIL_IN_CHUNK = 250
+SPAM_RATE = 0.1
+FLOOD_RATE_PER_CHUNK = 0.1
+SLEEP_TIME_IN_S = 1
 
 
-def toDict(x): return x.__dict__
+def run():
 
+    def mkdir_if_not_exists(path: Path):
+        if not path.exists():
+            path.mkdir(parents=True)
 
-if __name__ == "__main__":
-    DATA_PATH = Path("./data")
-    CHUNK_AMOUNT_LIMIT = 100
-    EMAIL_ADRESS_AMOUNT = 100
-    EMAIL_IN_CHUNK = 250
-    SPAM_RATE = 0.1
-    FLOOD_RATE_PER_CHUNK = 0.1
-    SLEEP_TIME_IN_S = 1
+    def get_latest_filename(path: Path) -> int:
+        return max(1, len(os.listdir(path)))
+
+    def waitIfFileChunkLimitReached(path: Path, limit: int, sleep_time: int):
+        while True:
+            file_in_queue = len(os.listdir(DATA_PATH))
+
+            if file_in_queue >= limit:
+                print('Sleeping...')
+                time.sleep(sleep_time)
+            else:
+                return
+
+    def toDict(x): return x.__dict__
 
     # Set up
     emailsAdresses = generateEmailAdresses(EMAIL_ADRESS_AMOUNT)
@@ -59,3 +56,7 @@ if __name__ == "__main__":
 
         print(f'{email_chunk} in {time.time() - startTime}')
         email_chunk += 1
+
+
+if __name__ == "__main__":
+    run()
