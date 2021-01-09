f609efc6091d1dcfe0bd6fb7d8e2ac42e333d0cf
Lyokolux <lyokolux@protonmail.com>
2021-01-09 17:40:46 +0100

    style(CassandraWrapper.py)

--- batch/CassandraWrapper.py
+++ batch/CassandraWrapper.py
@@ -1,8 +1,9 @@
 # pip install cassandra-driver
 from cassandra.cluster import Cluster
 
+
 class CassandraWrapper:
-  def __init__(self):
-    self.cluster = Cluster(['127.0.0.1'],port=9042)
-    self.session = self.cluster.connect('lambda',wait_for_all_pools=True)
-    self.session.execute('USE lambda')
+    def __init__(self):
+        self.cluster = Cluster(['127.0.0.1'], port=9042)
+        self.session = self.cluster.connect('lambda', wait_for_all_pools=True)
+        self.session.execute('USE lambda')
