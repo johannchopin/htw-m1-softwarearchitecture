785948dd04f8e518291e3eb41799b7d65826b283
Lyokolux <lyokolux@protonmail.com>
2021-01-10 16:23:56 +0100

    🔥 cassandra-entrypoint.sh

--- src/batch/cassandra-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-$CASSANDRA_HOME/bin/cassandra -fR
-cqlsh --execute "create keyspace lambda WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3};"
-echo "Create keyspace: $?"
-cqlsh --execute "create table lambda.emails ( id TEXT PRIMARY KEY, sender TEXT, receiver TEXT, timestamp TIMESTAMP, subject TEXT, body TEXT);"
-echo "Create email table: $?"
