3f7515ebbf50308c2bb62f5e9156c7a49e70052a
Lyokolux <lyokolux@protonmail.com>
2021-01-10 16:25:52 +0100

    ♻️(CassandraWrapper.py): _setup_db method

--- src/batch/CassandraWrapper.py
+++ src/batch/CassandraWrapper.py
@@ -9,19 +9,23 @@ class CassandraWrapper:
     def __init__(self):
         self.cluster = Cluster(['127.0.0.1'], port=9042)
         self.session = self.cluster.connect('', wait_for_all_pools=True)
+        self._setup_db()
+
+    def execute(self, query):
+        self.session.execute(query)
+
+    def _setup_db(self):
         self._execute_silently(
             "create keyspace lambda WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3};")
         self.execute('USE lambda')
         self._execute_silently(
             "create table emails ( id TEXT PRIMARY KEY, sender TEXT, receiver TEXT, timestamp TIMESTAMP, subject TEXT, body TEXT);")
 
-    def execute(self, query):
-        self.session.execute(query)
-
     def _execute_silently(self, query):
         try:
             self.execute(query)
         except AlreadyExists:
+            # Silent error
             pass
 
 
