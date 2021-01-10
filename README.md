066b110122482afabe806f377843ead6c6a0033d
Lyokolux <lyokolux@protonmail.com>
2021-01-10 15:09:36 +0100

    ♻️(BatchLayer): set up cassandra DB in CassandraWrapper.py

--- README.md
+++ README.md
@@ -11,27 +11,10 @@ source .venv/bin/activate
 python -m pip install -r requiremetns.txt
 # Export dependancies
 python -m pip freeze > requirements.txt
-# Exit venv
+# Exit venvdocker
 deactivate
 ```
 
 ### Batch Layer
 Build cassandra image: `docker build -t cassandra-batch src/batch`  
 Run cassandra image: `docker run -d -p 9042:9042 --name cassandra_b cassandra-batch`  
-Set up image: 
-```bash
-docker exec -it cassandra-batch bash
-cqlsh
-# create cassandra's keyspace
-create keyspace lambda WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3};
-use lambda;
-# create table emails
-create table emails ( id TEXT PRIMARY KEY, sender TEXT, receiver TEXT, timestamp TIMESTAMP, subject TEXT, body TEXT);
-exit
-exit
-```
-Or fast
-```bash
-docker exec -it cassandra-batch bash
-cqlsh --execute "create keyspace lambda WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3}; use lambda; create table emails ( id TEXT PRIMARY KEY, sender TEXT, receiver TEXT, timestamp TIMESTAMP, subject TEXT, body TEXT);"
-```
--- src/batch/CassandraWrapper.py
+++ src/batch/CassandraWrapper.py
@@ -1,4 +1,5 @@
 # pip install cassandra-driver
+from cassandra import AlreadyExists
 from cassandra.cluster import Cluster
 from .singleton import singleton
 
@@ -7,11 +8,21 @@ from .singleton import singleton
 class CassandraWrapper:
     def __init__(self):
         self.cluster = Cluster(['127.0.0.1'], port=9042)
-        self.session = self.cluster.connect('lambda', wait_for_all_pools=True)
-        self.session.execute('USE lambda')
+        self.session = self.cluster.connect('', wait_for_all_pools=True)
+        self._execute_silently(
+            "create keyspace lambda WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3};")
+        self.execute('USE lambda')
+        self._execute_silently(
+            "create table emails ( id TEXT PRIMARY KEY, sender TEXT, receiver TEXT, timestamp TIMESTAMP, subject TEXT, body TEXT);")
 
     def execute(self, query):
         self.session.execute(query)
+
+    def _execute_silently(self, query):
+        try:
+            self.execute(query)
+        except AlreadyExists:
+            pass
 
 
 if __name__ == "__main__":
@@ -19,4 +30,3 @@ if __name__ == "__main__":
     cassandra = CassandraWrapper()
     cassandra2 = CassandraWrapper()
     assert cassandra is cassandra2
-    print(cassandra.execute())
