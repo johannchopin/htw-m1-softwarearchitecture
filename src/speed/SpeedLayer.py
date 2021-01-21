ec581a5a757d02fbf1ea66d7cd3d06d116bdf2c4
Lyokolux <lyokolux@protonmail.com>
2021-01-21 16:59:53 +0100

    ♻️(SpeedLayer): refactor more shit

--- src/speed/SpeedLayer.py
+++ src/speed/SpeedLayer.py
@@ -6,8 +6,8 @@ from ..serving.CassandraViews import CassandraViewsInstance
 class SpeedLayer:
     def __init__(self):
         self.cassandra = CassandraViewsInstance
-        self.dictionary = self.read_suspicious_keywords_dictionnary(
-            self.get_keywords_filepath())
+        self.keywordSourceFile = self.get_keywords_filepath()
+        self.keyword_list = self._read_spam_keywords(self.keywordSourceFile)
 
     def get_keywords_filepath(self):
         dirname = os.path.dirname(__file__)
@@ -17,11 +17,15 @@ class SpeedLayer:
         # print(self.is_spam(email))
         pass
 
+    def _read_spam_keywords(self, filePath) -> List[str]:
+        with open(filePath, 'r') as f:
+            return [keyword for keyword in f if keyword]
+
     def is_spam(self, email):
         is_sender_in_spam_view = self.is_sender_in_spam_view(
             email['sender'])
         is_spam_by_wordlist = self.is_spam_by_wordlist(
-            self.dictionary, email)
+            self.keyword_list, email)
         return is_sender_in_spam_view or is_spam_by_wordlist
 
     def is_sender_in_spam_view(self, email):
@@ -29,10 +33,6 @@ class SpeedLayer:
         SELECT email from spams where email='{email}';
         """)
         return len(matching_email_count._current_rows)
-
-    def read_suspicious_keywords_dictionnary(self, filePath) -> List[str]:
-        with open(filePath, 'r') as f:
-            return [keyword for keyword in f if keyword]
 
     def is_spam_by_wordlist(self, list_dictionary, email):
         for item in list_dictionary:
