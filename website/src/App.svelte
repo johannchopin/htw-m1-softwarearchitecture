19b385a12e75ee4d0c8f499c5d33fb6aee187a10
johannchopin <johannchopin@pm.me>
2021-01-22 19:07:41 +0100

    🚧 Fetch spams

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -1,10 +1,32 @@
 <script>
-	export let name;
+	import { onMount } from 'svelte';
+	const API_ROOT = 'http://localhost:2020'
+	let spams = [];
+
+	const fetchSpams = () => {
+		fetch(`${API_ROOT}/spams`).then((response) => {
+			console.log(response.body)
+		}).catch(() => {
+			spams = ["test@test.com", "test2@test.com"]
+		})
+	}
+
+	onMount(async () => {
+		fetchSpams()
+		const intervalInstance = setInterval(() => {
+			fetchSpams()
+		}, 5000)
+	})
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	Hi penis
+	<ul id="spams" class="list-group">
+		{#each spams as spam}
+			<li class="list-group-item">{spam}</li>
+		{/each}
+	</ul>
 </main>
 
 <style>
@@ -13,13 +35,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
--- website/src/main.js
+++ website/src/main.js
@@ -1,5 +1,6 @@
-import App from './App.svelte';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
