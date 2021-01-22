bf3f103baa9d640b9acb944366a988954635db6d
johannchopin <johannchopin@pm.me>
2021-01-22 22:09:51 +0100

    ✨ Add API /spams/count route

--- src/app.py
+++ src/app.py
@@ -1,21 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify
 from .serving.CassandraWrapper import CassandraWrapper
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 cassandra = CassandraWrapper()
 
-@app.route('/spams')
-def hello_world():
-    spams = {}
-    spamsResponse = cassandra.execute("select * from spamsCounterLog;")
+def getSpamsFromView():
+    return cassandra.execute("select * from spamsCounterLog;")
+
+@app.route('/spams/count')
+def spams():
+    spams = []
+    spamsResponse = getSpamsFromView()
     spamsResponseCount = len(spamsResponse._current_rows)
 
     for i in range(spamsResponseCount):
+        spam = {}
         spamResponse = spamsResponse._current_rows[i]
         timestamp = spamResponse.timestamp
         count = spamResponse.spamcount
-        spams[timestamp] = count
+        spam["timestamp"] = timestamp
+        spam["count"] = count
+        spams.append(spam)
 
-    return json.dumps(spams)
+    return jsonify(spams)
+
+@app.route('/spams/count')
+def spamsCounter():
+    spamsResponse = getSpamsFromView()
+    spamsResponseCount = len(spamsResponse._current_rows)
+
+    return jsonify({"count": spamsResponseCount})
