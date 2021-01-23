b0c84ee85c7f29bdeb27649491ebd78659575ca2
johannchopin <johannchopin@pm.me>
2021-01-23 17:09:26 +0100

    ✨ Add API /speed/spams/count route

--- src/app.py
+++ src/app.py
@@ -13,6 +13,8 @@ def getSpamsFromView():
     return servingCassandra.execute("select * from spamsCounterLog;")
 def getEmailsCountNumber():
     return batchCassandra.execute("select count(*) from emails;")._current_rows[0].count
+def getSpamsAmountDetectedBySpeed():
+    return servingCassandra.execute("select * from spamAmountDetectedBySpeed;")
 
 @app.route('/spams/count')
 def spams():
@@ -30,6 +32,22 @@ def spams():
         spams.append(spam)
 
     return jsonify(spams)
+
+
+@app.route('/speed/spams/count')
+def spamsAmountDetectedBySpeed():
+    counts = []
+    spamsDetectedResponse = getSpamsAmountDetectedBySpeed()
+    spamsDetectedResponseCount = len(spamsDetectedResponse._current_rows)
+
+    for i in range(spamsDetectedResponseCount):
+        spamResponse = spamsDetectedResponse._current_rows[i]
+        count = spamResponse.spamcount
+        counts.append(count)
+
+    counts.sort()
+    latestCountValue = counts[-1]
+    return jsonify({"count": latestCountValue})
 
 @app.route('/emails/count')
 def emailsCount():
