705b6eecbb6dfa440be33664c5ce26090783c3a8
johannchopin <johannchopin@pm.me>
2021-01-24 13:14:16 +0100

    ✨ View for spam emails detected by batch

--- src/serving/CassandraViews.py
+++ src/serving/CassandraViews.py
@@ -4,7 +4,8 @@ from .CassandraWrapper import CassandraWrapper
 
 class CassandraViews (CassandraWrapper):
     LOG_TABLE = 'spamsCounterLog'
-    SPAM_AMOUNT_DETECTED_BY_SPEED_TABLE = 'spamAmountDetectedBySpeed'
+    SPAM_AMOUNT_DETECTED_BY_SPEED_TABLE = 'spamEmailAmountDetectedBySpeed'
+    SPAM_AMOUNT_DETECTED_BY_BATCH_TABLE = 'spamEmailAmountDetectedByBatch'
 
     def __init__(self):
         self.spamsViewTableCounter = 0
@@ -15,6 +16,8 @@ class CassandraViews (CassandraWrapper):
             f"create table {CassandraViews.LOG_TABLE} (timestamp TEXT PRIMARY KEY, spamCount BIGINT)")  # Related to addSpamLog
         self._execute_silently(
             f"create table {CassandraViews.SPAM_AMOUNT_DETECTED_BY_SPEED_TABLE} (timestamp TEXT PRIMARY KEY, spamCount BIGINT)")
+        self._execute_silently(
+            f"create table {CassandraViews.SPAM_AMOUNT_DETECTED_BY_BATCH_TABLE} (timestamp TEXT PRIMARY KEY, spamCount BIGINT)")
 
     def init_next_table(self):
         self._execute_silently(
@@ -42,5 +45,9 @@ class CassandraViews (CassandraWrapper):
         self.execute(
             f"INSERT INTO {CassandraViews.SPAM_AMOUNT_DETECTED_BY_SPEED_TABLE}(timestamp, spamCount) VALUES ('{now}', {amount})")
 
+    def addSpamAmountDetectedByBatch(self, amount):
+        now = int(time() * 10**6)
+        self.execute(
+            f"INSERT INTO {CassandraViews.SPAM_AMOUNT_DETECTED_BY_BATCH_TABLE}(timestamp, spamCount) VALUES ('{now}', {amount})")
 
 CassandraViewsInstance = CassandraViews()
